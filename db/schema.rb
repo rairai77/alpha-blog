bc99a8d9a92f4706396ab0242316f3be4ef1c60a
Raahil Jain <cutenitki@gmail.com>
2018-12-19 17:23:35 -0800

    added updated_at and created_at columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181220011538) do
+ActiveRecord::Schema.define(version: 20181220012137) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
